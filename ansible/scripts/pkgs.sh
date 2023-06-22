d6421a4cd1f7f7cbd49ef37f9ea915bf91afb68e
garrett <garrett@fluidnumerics.com>
2023-06-22 19:56:56 +0000

    Added lines to fix VS Code installation

--- ansible/scripts/pkgs.sh
+++ ansible/scripts/pkgs.sh
@@ -8,4 +8,7 @@ yum-config-manager --enable rhel-server-rhscl-7-rpms
 
 yum install -y devtoolset-7
 
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 yum install -y code
