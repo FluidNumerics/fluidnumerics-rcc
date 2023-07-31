9e3c95a4db4f4cd267da9494a5fd508124c04986
garrett <garrett@fluidnumerics.com>
2023-07-31 15:40:48 +0000

    Added docker installation lines

--- ansible/scripts/pkgs.sh
+++ ansible/scripts/pkgs.sh
@@ -12,3 +12,8 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 yum install -y code
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
